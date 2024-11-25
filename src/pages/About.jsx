c57b9997d554e85df4d8f193c7de5db56866db00
Alberto Monaco <alberto.monaco84@gmail.com>
2024-11-25 14:54:59 +0100

    add paragraph

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,6 +5,7 @@ export default function About() {
 			<Navbar />
 			<div className='container'>
 				<h1 className='my-4 text-center'>Chi Siamo</h1>
+				<p className='my-4 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
 				<p className='my-4 text-center'>Copyright 2024</p>
 			</div>
 		</>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,8 +6,8 @@ export default function Home() {
 			<Navbar />
 			<div className='container'>
 				<h1 className='my-4 text-center'>Homepage</h1>
-
 				<p className=' my-4 text-center'>Benvenuto nel mio blog!</p>
+				<p className='my-4 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
 			</div>
 		</>
 	)
